9d6015d2e668a79f51b9a55f69294a84a8d285e1
suagnes <suagnes@163.com>
2018-12-07 18:52:58 +0800

    Site updated: 2018-12-07 18:52:57

--- dist/music.js
+++ dist/music.js
@@ -19,18 +19,18 @@ const ap = new APlayer({
             lrc:'/dist/lrc/bomo.lrc'
         },
         {
-            name: '願い~あの頃のキミへ~ Acoustic ver.',
-            artist: '當山みれい',
-            url: 'https://m10.music.126.net/20181207191618/05c257f354314f9ffc567a8dce981f83/ymusic/e983/4940/535e/a4b161ab07369cf0e7c214e2fe45cfaa.mp3',
-            cover: 'https://p1.music.126.net/wRrEvenyms1DxFfTEiwE_Q==/109951163180867808.jpg?param=130y130',
-            lrc:'/dist/lrc/negaiJP.lrc'
-        },
-        {
             name: 'Wait',
             artist: 'NoMBe',
             url: 'https://m10.music.126.net/20181207190119/3707249a646ea978bcfacb35c16a94d6/ymusic/c1fb/5cb4/3211/cf3fc3f594b616d9217c12a816b1cd86.mp3',
             cover: 'https://p2.music.126.net/BMi28upQKHs1kvXteHiotg==/18705991325076241.jpg?param=130y130',
             lrc:'/dist/lrc/wait.lrc'
+        },
+        {
+            name: '願い~あの頃のキミへ~ Acoustic ver.',
+            artist: '當山みれい',
+            url: 'https://m10.music.126.net/20181207191618/05c257f354314f9ffc567a8dce981f83/ymusic/e983/4940/535e/a4b161ab07369cf0e7c214e2fe45cfaa.mp3',
+            cover: 'https://p1.music.126.net/wRrEvenyms1DxFfTEiwE_Q==/109951163180867808.jpg?param=130y130',
+            lrc:'/dist/lrc/negaiJP.lrc'
         },
     ]
 });
