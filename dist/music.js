3fa6c4dc6a6647bef20ab3b64e1a56c271cecc76
suagnes <suagnes@163.com>
2018-12-07 18:46:39 +0800

    Site updated: 2018-12-07 18:46:39

--- dist/music.js
+++ dist/music.js
@@ -18,13 +18,7 @@ const ap = new APlayer({
             cover: 'https://p1.music.126.net/2cYOs5q1syDBPMk-9rdIBA==/3416182632954257.jpg?param=90y90',
             lrc:'/dist/lrc/bomo.lrc'
         },
-        {
-            name: 'Wait',
-            artist: 'NoMBe',
-            url: 'https://m10.music.126.net/20181207190119/3707249a646ea978bcfacb35c16a94d6/ymusic/c1fb/5cb4/3211/cf3fc3f594b616d9217c12a816b1cd86.mp3',
-            cover: 'https://p2.music.126.net/BMi28upQKHs1kvXteHiotg==/18705991325076241.jpg?param=130y130',
-            lrc:'/dist/lrc/wait.lrc'
-        },
+      
         // {
         //     name: 'SPOILER [JAPANESE MIX]',
         //     artist: 'Epik High / 南太铉',
@@ -38,6 +32,13 @@ const ap = new APlayer({
             url: 'https://m10.music.126.net/20181207184105/5a50d189ada4bc9e3855f6d673a1da17/ymusic/e983/4940/535e/a4b161ab07369cf0e7c214e2fe45cfaa.mp3',
             cover: 'https://p1.music.126.net/wRrEvenyms1DxFfTEiwE_Q==/109951163180867808.jpg?param=130y130',
             lrc:'/dist/lrc/negaiJP.lrc'
+        },
+        {
+            name: 'Wait',
+            artist: 'NoMBe',
+            url: 'https://m10.music.126.net/20181207190119/3707249a646ea978bcfacb35c16a94d6/ymusic/c1fb/5cb4/3211/cf3fc3f594b616d9217c12a816b1cd86.mp3',
+            cover: 'https://p2.music.126.net/BMi28upQKHs1kvXteHiotg==/18705991325076241.jpg?param=130y130',
+            lrc:'/dist/lrc/wait.lrc'
         },
     ]
 });
